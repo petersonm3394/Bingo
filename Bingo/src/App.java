551c6a5175531324ad5de336ccb580741687fe49
Marcus <petersonm3394@gmail.com>
2025-01-31 14:53:11 -0600

    Basic structure of bingo game

--- Bingo/src/App.java
+++ Bingo/src/App.java
@@ -1,5 +1,23 @@
+import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numCards = 0;
+        boolean cheats = false;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Bingo Game!");
+        System.out.println("Please enter the number of cards (1-4)-> " );
+        numCards = sc.nextInt();
+        System.out.println("With cheats? (Y/N)-> " );
+        String cheat = sc.next();
+        if(cheat.equals("Y") || cheat.equals("y")){
+            cheats = true;
+        }
+        else if (cheat.equals("N") || cheat.equals("n")){
+            cheats = false;
+        }
+        else{
+            System.out.println("Invalid input. Please enter Y or N.");
+        }
+        System.out.println("Selected: " + numCards + " cards and cheats: " + cheats);
     }
 }
