b36da9629b66e3965a588fc4ed522960ce520e83
Marcus <petersonm3394@gmail.com>
2025-02-02 22:14:28 -0600

    testing with 2d object of slots

--- Bingo/src/App.java
+++ Bingo/src/App.java
@@ -1,23 +1,39 @@
-import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        int numCards = 0;
-        boolean cheats = false;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to Bingo Game!");
-        System.out.println("Please enter the number of cards (1-4)-> " );
-        numCards = sc.nextInt();
-        System.out.println("With cheats? (Y/N)-> " );
-        String cheat = sc.next();
-        if(cheat.equals("Y") || cheat.equals("y")){
-            cheats = true;
+
+        Slot[][] slots = new Slot[5][5];
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                slots[i][j] = new Slot(i, j, i*5 + j);
+            }
         }
-        else if (cheat.equals("N") || cheat.equals("n")){
-            cheats = false;
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                slots[i][j].printSlot();
+            }
+            System.out.println();
         }
-        else{
-            System.out.println("Invalid input. Please enter Y or N.");
-        }
-        System.out.println("Selected: " + numCards + " cards and cheats: " + cheats);
+        // int numCards = 0;
+        // boolean cheats = false;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Welcome to Bingo Game!");
+        // System.out.println("Please enter the number of cards (1-4)-> " );
+        // numCards = sc.nextInt();
+        // System.out.println("With cheats? (Y/N)-> " );
+        // String cheat = sc.next();
+        // if(cheat.equals("Y") || cheat.equals("y")){
+        //     cheats = true;
+        // }
+        // else if (cheat.equals("N") || cheat.equals("n")){
+        //     cheats = false;
+        // }
+        // else{
+        //     System.out.println("Invalid input. Please enter Y or N.");
+        // }
+        // System.out.println("Selected: " + numCards + " cards and cheats: " + cheats);
+
+        // sc.close();
     }
+
+    
 }
