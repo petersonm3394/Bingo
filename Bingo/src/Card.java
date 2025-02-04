cda1f88d0029e34cef04a0c55ac5b0203e479191
Marcus <petersonm3394@gmail.com>
2025-02-03 20:26:57 -0600

    fixed card class

--- Bingo/src/Card.java
+++ Bingo/src/Card.java
@@ -1,33 +1,39 @@
 
 
 public class Card {
-private Slot[][] pos = new Slot[5][5];
+    private Slot[][] slots = new Slot[5][5];
 
-//constructor
-public Card(Slot[][] pos){
-    this.pos = pos;
-}
-//default constructor
-public Card() {
-    for(int i = 0; i < 5; i++){
-        for(int j = 0; j < 5; j++){
-            pos[i][j] = new Slot();
+    //constructor 
+    public Card(Slot[][] slots_) {
+        this.slots = slots_;
+    }
+    public Card() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                slots[i][j] = new Slot();
+            }
         }
     }
-}
-
-public Slot getSlot(int x, int y) {
-    return pos[x][y];
-}
-
-public void printCard(){
-    for (int i = 0; i < 5; i++){
-        System.out.print("| ");
-        for (int j = 0; j < 5; j++){
-            pos[i][j].printSlot();
+    public void printCard() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                slots[i][j].printSlot();
+            }
+            System.out.println();
         }
-        System.out.println();
     }
-}
+    public boolean markCard(int x, int y) {
+        if(getSlotPos(x, y) == -1) {
+            return false;
+        }
+        else {
+            slots[x][y].mark();
+            return true;
+        }
+    }
+    public int getSlotPos(int x, int y) {
+        return slots[x][y].getValue();
+    }
 
 }
+
