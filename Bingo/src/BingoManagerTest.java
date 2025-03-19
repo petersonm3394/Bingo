e2c9e6fce2b94ff06ba9ca5ff83ca35a05d1f38e
Marcus <petersonm3394@gmail.com>
2025-03-19 16:42:20 -0500

    Tests 1-4 passed. Works for multiple types of patterns added for bingo checks

--- Bingo/src/BingoManagerTest.java
+++ Bingo/src/BingoManagerTest.java
@@ -56,6 +56,48 @@ public class BingoManagerTest {
 
     }
 
-    
+    @Test
+    public void testOddRowsColumn() {
+        Card c3 = new Card();
+
+        for (int i = 0; i < 5; i++) { //marks odd rows and last column
+            c3.markCardPos(0, i);
+            c3.markCardPos(2, i);
+            c3.markCardPos(4, i);
+            c3.markCardPos(i, 4);
+        }
+
+        Pattern p3 = new Pattern(c3);
+        BingoManager BM3 = new BingoManager();
+        BM3.addPattern(BingoManager.patType.ROW);
+
+        ArrayList<BingoManager.patType> myList = BM3.getPatList();
+        
+        assertEquals(3,BM3.computeBingo(myList, p3)); //output should be 3 since the column isn't counted
+
+    }
+
+    @Test
+    public void testOddRowswColumn() {
+        Card c4 = new Card();
+
+        for (int i = 0; i < 5; i++) { //marks odd rows and last column
+            c4.markCardPos(0, i);
+            c4.markCardPos(2, i);
+            c4.markCardPos(4, i);
+            c4.markCardPos(i, 4);
+        }
+
+        Pattern p4 = new Pattern(c4);
+        BingoManager BM4 = new BingoManager();
+        BM4.addPattern(BingoManager.patType.ROW);
+        BM4.addPattern(BingoManager.patType.COLUMN);
+
+        ArrayList<BingoManager.patType> myList = BM4.getPatList();
+        
+        assertEquals(4,BM4.computeBingo(myList, p4));
+    }
+
+
 
 }
